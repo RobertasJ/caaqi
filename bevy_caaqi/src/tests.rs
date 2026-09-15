1c5ba7141ce3bc3263a34f30866d674ed01c5a2e
RobertasJ <robertasj8@gmail.com>
2026-09-15 17:39:05 +0200

    better test tools

--- bevy_caaqi/src/tests.rs
+++ bevy_caaqi/src/tests.rs
@@ -1,4 +1,4 @@
-use bevy::{log::LogPlugin, prelude::*};
+use bevy::prelude::*;
 use googletest::prelude::*;
 
 use crate::{
@@ -10,10 +10,30 @@ use crate::{
     tracked_value::create_ref,
 };
 
+#[derive(Resource, Default)]
+struct TestTrace(Vec<TestEvent>);
+
+#[derive(Debug, PartialEq, Eq)]
+enum TestEvent {
+    Read(i32),
+    Ran(&'static str),
+}
+
+fn record(world: &mut World, event: TestEvent) {
+    world.resource_mut::<TestTrace>().0.push(event);
+}
+
+#[track_caller]
+fn expect_trace(world: &mut World, expected: &[TestEvent]) {
+    let trace = &world.resource::<TestTrace>().0;
+    expect_eq!(trace, expected);
+}
+
 fn testing_app() -> App {
     init_test_logging();
     let mut app = App::new();
     app.add_plugins(CaaqiPlugin);
+    app.init_resource::<TestTrace>();
     app
 }
 
@@ -40,13 +60,11 @@ fn test_empty() {
     let mut app = testing_app();
     let world = app.world_mut();
 
-    let mut root_run_count = create_ref(&mut *world, 0);
-
     test_eval(world, move |world| {
-        *root_run_count.silent_write(&mut *world) += 1;
+        record(world, TestEvent::Ran("root"));
     });
 
-    expect_eq!(*root_run_count.silent_read(&mut *world), 1);
+    expect_trace(world, &[TestEvent::Ran("root")]);
 }
 
 #[gtest]
@@ -54,30 +72,32 @@ fn test_order() {
     let mut app = testing_app();
     let world = app.world_mut();
 
-    let mut root_run_count = create_ref(world, 0);
-    let mut first_run_count = create_ref(world, 0);
-    let mut second_run_count = create_ref(world, 0);
-
     test_eval(world, move |world| {
-        *root_run_count.silent_write(&mut *world) += 1;
+        record(world, TestEvent::Ran("root"));
 
         action(world, move |world| {
-            expect_eq!(*root_run_count.silent_read(&mut *world), 1);
-            *first_run_count.silent_write(&mut *world) += 1;
+            record(world, TestEvent::Ran("action 1"));
         });
+
+        record(world, TestEvent::Ran("between"));
 
         action(world, move |world| {
-            expect_eq!(*first_run_count.silent_read(&mut *world), 1);
-            *second_run_count.silent_write(&mut *world) += 1;
+            record(world, TestEvent::Ran("action 2"));
         });
 
-        expect_eq!(*second_run_count.silent_read(&mut *world), 1);
-        expect_eq!(*first_run_count.silent_read(&mut *world), 1);
+        record(world, TestEvent::Ran("root end"));
     });
 
-    expect_eq!(*root_run_count.silent_read(&mut *world), 1);
-    expect_eq!(*first_run_count.silent_read(&mut *world), 1);
-    expect_eq!(*second_run_count.silent_read(&mut *world), 1);
+    expect_trace(
+        world,
+        &[
+            TestEvent::Ran("root"),
+            TestEvent::Ran("action 1"),
+            TestEvent::Ran("between"),
+            TestEvent::Ran("action 2"),
+            TestEvent::Ran("root end"),
+        ],
+    );
 }
 
 #[gtest]
@@ -85,36 +105,28 @@ fn test_read_write() {
     let mut app = testing_app();
     let world = app.world_mut();
 
-    let mut root_run_count = create_ref(world, 0);
-    let mut read_run_count = create_ref(world, 0);
-    let mut write_run_count = create_ref(world, 0);
-
     test_eval(world, move |world| {
-        *root_run_count.silent_write(&mut *world) += 1;
-
         let mut val = create_ref(&mut *world, 0);
 
         action(world, move |world| {
-            println!("{}", *val.read(&mut *world));
-            *read_run_count.silent_write(&mut *world) += 1;
-
-            match *read_run_count.silent_read(&mut *world) {
-                1 => expect_eq!(*val.silent_read(&mut *world), 0),
-                2 => expect_eq!(*val.silent_read(&mut *world), 1),
-                _ => {}
-            }
+            let read = val.read(&mut *world);
+            record(world, TestEvent::Read(*read));
         });
 
         action(world, move |world| {
             *val.write(&mut *world) += 1;
-            println!("wrote val");
-            *write_run_count.silent_write(&mut *world) += 1;
+            record(world, TestEvent::Ran("write"));
         });
     });
 
-    expect_eq!(*root_run_count.silent_read(&mut *world), 1);
-    expect_eq!(*read_run_count.silent_read(&mut *world), 2);
-    expect_eq!(*write_run_count.silent_read(&mut *world), 1);
+    expect_trace(
+        world,
+        &[
+            TestEvent::Read(0),
+            TestEvent::Ran("write"),
+            TestEvent::Read(1),
+        ],
+    );
 }
 
 #[gtest]
@@ -122,34 +134,21 @@ fn test_write_read() {
     let mut app = testing_app();
     let world = app.world_mut();
 
-    let mut root_run_count = create_ref(world, 0);
-    let mut read_run_count = create_ref(world, 0);
-    let mut write_run_count = create_ref(world, 0);
-
     test_eval(world, move |world| {
-        *root_run_count.silent_write(&mut *world) += 1;
-
         let mut val = create_ref(&mut *world, 0);
 
         action(world, move |world| {
             *val.write(&mut *world) += 1;
-            println!("wrote val");
-            *write_run_count.silent_write(&mut *world) += 1;
+            record(world, TestEvent::Ran("write"));
         });
 
         action(world, move |world| {
-            println!("{}", *val.read(&mut *world));
-            *read_run_count.silent_write(&mut *world) += 1;
-
-            if *read_run_count.silent_read(&mut *world) == 1 {
-                expect_eq!(*val.silent_read(&mut *world), 1);
-            }
+            let read = val.read(&mut *world);
+            record(world, TestEvent::Read(*read));
         });
     });
 
-    expect_eq!(*root_run_count.silent_read(&mut *world), 1);
-    expect_eq!(*read_run_count.silent_read(&mut *world), 1);
-    expect_eq!(*write_run_count.silent_read(&mut *world), 1);
+    expect_trace(world, &[TestEvent::Ran("write"), TestEvent::Read(1)]);
 }
 
 #[gtest]
@@ -157,44 +156,33 @@ fn test_single_rewind() {
     let mut app = testing_app();
     let world = app.world_mut();
 
-    let mut root_run_count = create_ref(world, 0);
-    let mut read_run_count = create_ref(world, 0);
-    let mut write_run_count = create_ref(world, 0);
-    let mut rewind_run_count = create_ref(world, 0);
-
     test_eval(world, move |world| {
-        *root_run_count.silent_write(&mut *world) += 1;
-
         let mut val = create_ref(&mut *world, 0);
 
         action(world, move |world| {
-            println!("{}", *val.read(&mut *world));
-            *read_run_count.silent_write(&mut *world) += 1;
-
-            match *read_run_count.silent_read(&mut *world) {
-                1 => expect_eq!(*val.silent_read(&mut *world), 0),
-                2 => expect_eq!(*val.silent_read(&mut *world), 1),
-                _ => {}
-            }
+            let read = val.read(&mut *world);
+            record(world, TestEvent::Read(*read));
 
             rewind(world, move |world| {
-                *rewind_run_count.silent_write(&mut *world) += 1;
-                expect_eq!(*read_run_count.silent_read(&mut *world), 1);
-                println!("rewound");
+                record(world, TestEvent::Ran("rewound"));
             });
         });
 
         action(world, move |world| {
             *val.write(&mut *world) += 1;
-            println!("wrote val");
-            *write_run_count.silent_write(&mut *world) += 1;
+            record(world, TestEvent::Ran("wrote val"));
         });
     });
 
-    expect_eq!(*root_run_count.silent_read(&mut *world), 1);
-    expect_eq!(*read_run_count.silent_read(&mut *world), 2);
-    expect_eq!(*write_run_count.silent_read(&mut *world), 1);
-    expect_eq!(*rewind_run_count.silent_read(&mut *world), 1);
+    expect_trace(
+        world,
+        &[
+            TestEvent::Read(0),
+            TestEvent::Ran("wrote val"),
+            TestEvent::Ran("rewound"),
+            TestEvent::Read(1),
+        ],
+    );
 }
 
 #[gtest]
@@ -202,54 +190,43 @@ fn test_synced_rewinds() {
     let mut app = testing_app();
     let world = app.world_mut();
 
-    let mut root_run_count = create_ref(world, 0);
-    let mut read_run_count = create_ref(world, 0);
-    let mut write_run_count = create_ref(world, 0);
-    let mut rewind1_run_count = create_ref(world, 0);
-    let mut rewind2_run_count = create_ref(world, 0);
     let sync_key = create_sync_key(world);
 
     test_eval(world, move |world| {
-        *root_run_count.silent_write(&mut *world) += 1;
-
         let mut val = create_ref(&mut *world, 0);
 
         action(world, move |world| {
-            println!("{}", *val.read(&mut *world));
-            *read_run_count.silent_write(&mut *world) += 1;
-
-            match *read_run_count.silent_read(&mut *world) {
-                1 => expect_eq!(*val.silent_read(&mut *world), 0),
-                2 => expect_eq!(*val.silent_read(&mut *world), 1),
-                _ => {}
-            }
+            let read = *val.read(&mut *world);
+            record(world, TestEvent::Read(read));
 
             synced_rewind(world, [sync_key], move |world| {
-                expect_eq!(*rewind2_run_count.silent_read(&mut *world), 1);
-
-                *rewind1_run_count.silent_write(&mut *world) += 1;
-                println!("rewound 1");
+                record(world, TestEvent::Ran("rewound action 1"));
             });
         });
 
         action(world, move |world| {
+            record(world, TestEvent::Ran("ran action 2"));
             synced_rewind(world, [sync_key], move |world| {
-                expect_eq!(*rewind1_run_count.silent_read(&mut *world), 0);
-                *rewind2_run_count.silent_write(&mut *world) += 1;
-                println!("rewound 2");
+                record(world, TestEvent::Ran("rewound action 2"));
             });
         });
 
         action(world, move |world| {
             *val.write(&mut *world) += 1;
-            println!("wrote val");
-            *write_run_count.silent_write(&mut *world) += 1;
+            record(world, TestEvent::Ran("wrote val"));
         });
     });
 
-    expect_eq!(*root_run_count.silent_read(&mut *world), 1);
-    expect_eq!(*read_run_count.silent_read(&mut *world), 2);
-    expect_eq!(*write_run_count.silent_read(&mut *world), 1);
-    expect_eq!(*rewind1_run_count.silent_read(&mut *world), 1);
-    expect_eq!(*rewind2_run_count.silent_read(&mut *world), 1);
+    expect_trace(
+        world,
+        &[
+            TestEvent::Read(0),
+            TestEvent::Ran("ran action 2"),
+            TestEvent::Ran("wrote val"),
+            TestEvent::Ran("rewound action 2"),
+            TestEvent::Ran("rewound action 1"),
+            TestEvent::Read(1),
+            TestEvent::Ran("ran action 2"),
+        ],
+    );
 }
